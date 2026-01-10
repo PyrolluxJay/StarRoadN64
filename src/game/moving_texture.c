24a2439657f5a695fa1c9e719f934821d0c5ea76
aglab2 <aglab3@gmail.com>
2026-01-10 21:37:50 +0800

    Added movtex

--- src/game/moving_texture.c
+++ src/game/moving_texture.c
@@ -489,6 +489,7 @@ extern Movtex ttm_movtex_puddle[];
 /**
  * Find the quadCollection for a given quad collection id.
  */
+extern void *GetRomhackWaterBox(u32 id);
 void *get_quad_collection_from_id(u32 id) {
     switch (id) {
         case BBH_MOVTEX_MERRY_GO_ROUND_WATER_ENTRANCE:
@@ -539,8 +540,8 @@ void *get_quad_collection_from_id(u32 id) {
             return castle_courtyard_movtex_star_statue_water;
         case TTM_MOVTEX_PUDDLE:
             return ttm_movtex_puddle;
-        default:
-            return NULL;
+		default:
+            return GetRomhackWaterBox(id);
     }
 }
 
@@ -892,3 +893,61 @@ Gfx *geo_movtex_update_horizontal(s32 callContext, struct GraphNode *node, UNUSE
     }
     return NULL;
 }
+
+/*
+This is an include meant to help with the addition of moving textures for water boxes. Moving textures are hardcoded in vanilla, but in hacks they're procedural. Every hack uses 0x5000 +Type (0 for water, 1 for toxic mist, 2 for mist) to locate the tables for their water boxes. I will replicate this by using a 3 dimensional array of pointers. This wastes a little bit of memory but is way easier to manage.
+To use this, simply place this file inside your source directory after exporting.
+*/
+extern u8 bbh_1_Movtex_0[] = {};
+extern u8 ccm_1_Movtex_0[] = {};
+extern u8 hmc_1_Movtex_0[] = {};
+extern u8 ssl_1_Movtex_0[] = {};
+extern u8 bob_1_Movtex_0[] = {};
+extern u8 sl_1_Movtex_0[] = {};
+extern u8 jrb_1_Movtex_0[] = {};
+extern u8 castle_grounds_1_Movtex_0[];
+extern u8 lll_1_Movtex_0[] = {};
+extern u8 wf_1_Movtex_0[] = {};
+extern u8 ending_1_Movtex_0[] = {};
+extern u8 castle_courtyard_1_Movtex_0[] = {};
+extern u8 pss_1_Movtex_0[] = {};
+extern u8 cotmc_1_Movtex_0[] = {};
+extern u8 totwc_1_Movtex_0[] = {};
+extern u8 wmotr_1_Movtex_0[] = {};
+
+static void *RM2C_Water_Box_Array[33][8][3] = {
+    { {NULL,NULL,NULL,},{&bbh_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&ccm_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&hmc_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&ssl_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&bob_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&sl_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&jrb_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&castle_grounds_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&lll_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&wf_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&ending_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&castle_courtyard_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&pss_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&cotmc_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&totwc_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, },
+    { {NULL,NULL,NULL,},{&wmotr_1_Movtex_0,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,},{NULL,NULL,NULL,}, }
+};
+
+void *GetRomhackWaterBox(u32 id)
+{
+    id = id&0xF;
+    return RM2C_Water_Box_Array[gCurrLevelNum-4][gCurrAreaIndex][id];
+};
