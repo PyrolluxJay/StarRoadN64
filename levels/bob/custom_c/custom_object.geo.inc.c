b0ff09178f81250eb3ed055d6c222f1f4de19c3a
aglab2 <aglab3@gmail.com>
2026-01-11 12:22:13 +0800

    Set correct layers for bob

--- levels/bob/custom_c/custom_object.geo.inc.c
+++ levels/bob/custom_c/custom_object.geo.inc.c
@@ -2,7 +2,7 @@ extern Gfx DL_hmc_geo_000530_0x7020188[];
 const GeoLayout star_road_boi_rock_geo[]= {
     GEO_CULLING_RADIUS(1000),
     GEO_OPEN_NODE(),
-    GEO_DISPLAY_LIST(4,DL_hmc_geo_000530_0x7020188),
+    GEO_DISPLAY_LIST(LAYER_OPAQUE,DL_hmc_geo_000530_0x7020188),
     GEO_CLOSE_NODE(),
     GEO_END(),
 };
@@ -11,7 +11,7 @@ extern Gfx DL_hmc_geo_000570_0x7024048[];
 const GeoLayout star_road_boi_platform_geo[]= {
     GEO_CULLING_RADIUS(100),
     GEO_OPEN_NODE(),
-    GEO_DISPLAY_LIST(1,DL_hmc_geo_000570_0x7024048),
+    GEO_DISPLAY_LIST(LAYER_OPAQUE,DL_hmc_geo_000570_0x7024048),
     GEO_CLOSE_NODE(),
     GEO_END(),
 };
