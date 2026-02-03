64147abde20a0bf817fa1881e532fda7f722bb92
aglab2 <aglab3@gmail.com>
2026-02-03 08:50:55 +0800

    Fixed coral water in the cannon

--- levels/hmc/coral_water/header.h
+++ levels/hmc/coral_water/header.h
@@ -1,4 +1,4 @@
 extern u8 coral_water__02014AB8_ci8[];
 extern u8 coral_water__02014AB8_pal_rgba16[];
-extern Vtx coral_water_Plane_001_mesh_vtx_0[175];
+extern Vtx coral_water_Plane_001_mesh_vtx_0[162];
 extern Gfx coral_water_Plane_001_mesh[];
